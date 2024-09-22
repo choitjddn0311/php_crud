74a8e718310b9eca663d15f65582aa249089aab0
Sungwoo Choi <128563032+choitjddn0311@users.noreply.github.com>
2024-09-23 00:30:15 +0900

    feat : using while

--- select.php
+++ select.php
@@ -1,8 +1,21 @@
 <?php
 $conn = mysqli_connect('localhost' , 'root' , '' , 'opentutorials');
-$sql = "SELECT * FROM topic WHERE id = 7";
+
+//1 row
+echo "<h1>single row</h1>";
+$sql = "SELECT * FROM topic WHERE id = 8";
 $result = mysqli_query($conn, $sql);
 $row = mysqli_fetch_array($result);
-echo '<h1>'.$row['title'].'</h1>';
+echo '<h2>'.$row['title'].'</h2>';
 echo $row['description'];
+
+//all row
+echo "<h1>multi row</h1>";
+$sql = "SELECT * FROM topic";
+$result = mysqli_query($conn, $sql);
+
+while($row = mysqli_fetch_array($result)) {
+    echo '<h2>'.$row['title'].'</h2>';
+    echo $row['description'];
+}
 ?>
