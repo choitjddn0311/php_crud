64d9dcea94dc49b20c5fcceb0e0fa3e6e9602924
Sungwoo Choi <128563032+choitjddn0311@users.noreply.github.com>
2024-09-22 15:04:49 +0900

    feat : select and insert the html

--- select.php
+++ select.php
@@ -1,6 +1,8 @@
 <?php
 $conn = mysqli_connect('localhost' , 'root' , '' , 'opentutorials');
-$sql = "SELECT * FROM topic";
+$sql = "SELECT * FROM topic WHERE id = 7";
 $result = mysqli_query($conn, $sql);
-var_dump($result->num_rows);
+$row = mysqli_fetch_array($result);
+echo '<h1>'.$row['title'].'</h1>';
+echo $row['description'];
 ?>
