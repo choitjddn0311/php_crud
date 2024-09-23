4af11642d59b5f9501dafc0b72a10af8e1bb9bd3
choitjddn0311 <cswcjw311427@naver.com>
2024-09-23 18:07:47 +0900

    feat : using movement data

--- select.php
+++ select.php
@@ -3,7 +3,7 @@ $conn = mysqli_connect('localhost' , 'root' , '' , 'opentutorials');
 
 //1 row
 echo "<h1>single row</h1>";
-$sql = "SELECT * FROM topic WHERE id = 8";
+$sql = "SELECT * FROM topic WHERE id = 38";
 $result = mysqli_query($conn, $sql);
 $row = mysqli_fetch_array($result);
 echo '<h2>'.$row['title'].'</h2>';
