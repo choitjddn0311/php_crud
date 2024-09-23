22281db481b9289334832ab2af26c6f5d694b7dd
Sungwoo Choi <128563032+choitjddn0311@users.noreply.github.com>
2024-09-24 00:31:20 +0900

    feat : filtering

--- index.php
+++ index.php
@@ -13,6 +13,7 @@ $article = array(
     'description' => 'Hello, web'
 );
 if(isset($_GET['id'])) {
+    $filtered_id = mysqli_real_escape_string($conn ,$_GET['id']);
     $sql = "SELECT * FROM topic WHERE id={$_GET['id']}";
     $result = mysqli_query($conn,$sql);
     $row = mysqli_fetch_array($result);
--- process_create.php
+++ process_create.php
@@ -1,6 +1,10 @@
 <?php
 mysqli_report(MYSQLI_REPORT_OFF);
 $conn = mysqli_connect('localhost' , 'root' , '' , 'opentutorials');
+$filtered =  array(
+    'title'=>mysqli_real_escape_string($conn, $_POST['title']),
+    'description'=>mysqli_real_escape_string($conn, $_POST['description'])
+);
 // var_dump 는 변수형을 출력해주는 함수이다.
 // var_dump(($_POST));
 $sql = "insert into topic(title, description, created) 
